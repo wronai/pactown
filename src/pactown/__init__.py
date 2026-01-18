8a5b408de3f321cd406d95c4ee71a1da21502560
Tom Softreck <tom@sapletta.com>
2026-01-18 19:31:51 +0100

    release

--- src/pactown/__init__.py
+++ src/pactown/__init__.py
@@ -1,6 +1,6 @@
 """Pactown – Decentralized Service Ecosystem Orchestrator using markpact"""
 
-__version__ = "0.1.46"
+__version__ = "0.1.47"
 
 from .config import DependencyConfig, EcosystemConfig, ServiceConfig
 from .network import PortAllocator, ServiceEndpoint, ServiceRegistry, find_free_port
